5874e532bd84d171cf21aaecd51f3b1420885443
AHMED HAFDI <58536023+HAFDIAHMED@users.noreply.github.com>
2023-12-13 11:44:28 +0100

    integrate easier ide to debug AI
    integrate easier ide to debug AI

--- ide.py
+++ ide.py
@@ -1,7 +1,10 @@
 import os
 from tkinter import Tk, Text, Scrollbar, Menu, filedialog, StringVar, ttk, Listbox, END, SINGLE, messagebox
 import openai
-
+from sklearn.svm import LinearSVC
+from sklearn.pipeline import make_pipeline
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.feature_extraction.text import TfidfVectorizer
 class SimpleIDE:
     def __init__(self, root):
         self.root = root
@@ -39,6 +42,11 @@ class SimpleIDE:
         self.file_menu.add_separator()
         self.file_menu.add_command(label="Exit", command=self.exit_app)
 
+        self.debug_menu = Menu(self.menu, tearoff=False)
+        self.menu.add_cascade(label="Debug", menu=self.debug_menu)
+        # self.debug_menu.add_command(label="Debug with ChatGPT", command=self.debug_with_chatgpt)
+        self.debug_menu.add_command(label="Debug with AI", command=self.debug_code)
+
         self.help_menu = Menu(self.menu, tearoff=False)
         self.menu.add_cascade(label="Help", menu=self.help_menu)
         self.help_menu.add_command(label="About", command=self.show_about)
@@ -55,8 +63,46 @@ class SimpleIDE:
         openai.api_key = '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'
 
         # Add a menu item for debugging with ChatGPT
-        self.file_menu.add_command(label="Debug with ChatGPT", command=self.debug_with_chatgpt)
+        self.model = make_pipeline(TfidfVectorizer(), MultinomialNB())
 
+    def debug_code(self):
+            # Fit the model with data before calling predict
+            if self.open_files:
+                training_data = [self.get_file_content(file) for file in self.open_files]
+                labels = [1 if "error" in content.lower() else 0 for content in training_data]
+                self.model.fit(training_data, labels)
+
+                # Now you can call predict
+                code_text = self.text.get(1.0, "end-1c")
+                prediction = self.model.predict([code_text])
+                messagebox.showinfo("Debugging Result", f"Is there a syntax error? {'Yes' if prediction[0] == 1 else 'No'}")
+            else:
+                messagebox.showinfo("Debugging Result", "No training data available.")
+
+    def get_file_content(self, file_path):
+        with open(file_path, "r") as file:
+            return file.read()
+    def debug_with_chatgpt(self):
+        if self.current_file:
+            with open(self.current_file, "r") as file:
+                file_content = file.read()
+                debug_output = self.call_chatgpt(file_content)
+
+                # Display the ChatGPT debug output in a messagebox
+                messagebox.showinfo("Debug Output", debug_output)
+
+    def call_chatgpt(self, code):
+        # Call OpenAI API for debugging
+        response = openai.Completion.create(
+            engine="text-davinci-002",  # You can use other engines
+            prompt=code,
+            max_tokens=100
+        )
+
+        # Extract the generated text from ChatGPT response
+        debug_output = response.choices[0].text.strip()
+
+        return debug_output
 
     def set_window_title(self, name=None):
         title = "Easier IDE"
@@ -118,27 +164,6 @@ class SimpleIDE:
     def show_about(self):
         about_text = "Easier IDE\n\nA simple text editor with basic functionality.\nVersion: 1.0\n\nDeveloped by WebSolvus for debugging code with machine learning."
         messagebox.showinfo("About", about_text)
-    def debug_with_chatgpt(self):
-            if self.current_file:
-                with open(self.current_file, "r") as file:
-                    file_content = file.read()
-                    debug_output = self.call_chatgpt(file_content)
-
-                    # Display the ChatGPT debug output in a messagebox
-                    messagebox.showinfo("Debug Output", debug_output)
-
-    def call_chatgpt(self, code):
-        # Call OpenAI API for debugging
-        response = openai.Completion.create(
-            engine="text-davinci-002",  # You can use other engines
-            prompt=code,
-            max_tokens=100
-        )
-
-        # Extract the generated text from ChatGPT response
-        debug_output = response.choices[0].text.strip()
-
-        return debug_output
 
 if __name__ == "__main__":
     root = Tk()
